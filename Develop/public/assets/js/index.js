006a50cbbe33d90eb80edf6ac2511c7e133b8b9e
dcashdallas <dcashdallas81@yahoo.com>
2020-06-15 18:27:42 -0500

    adding variables and functions for index, delete function

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -24,3 +24,28 @@ var saveNote = function (note) {
   });
 };
 
+// A function for deleting a note from the db
+var deleteNote = function (id) {
+  return $.ajax({
+    url: "api/notes/" + id,
+    method: "DELETE"
+  });
+};
+
+// If there is an activeNote, display it, otherwise render empty inputs
+var renderActiveNote = function () {
+  $saveNoteBtn.hide();
+
+  if (activeNote.id) {
+    $noteTitle.attr("readonly", true);
+    $noteText.attr("readonly", true);
+    $noteTitle.val(activeNote.title);
+    $noteText.val(activeNote.text);
+  } else {
+    $noteTitle.attr("readonly", false);
+    $noteText.attr("readonly", false);
+    $noteTitle.val("");
+    $noteText.val("");
+  }
+};
+
