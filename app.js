0bed779ebdd983972a4e1a7366895de40298d7c9
dcashdallas <dcashdallas81@yahoo.com>
2020-06-14 21:24:21 -0500

    updating js data

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ const PORT = process.env.PORT || 2001;
 
 let notesData = [];
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "Develop/public")));
+
+
+app.get("/api/notes", function (err, res) {
+    try {
+        notesData = fs.readFileSync("Develop/db/db.json", "utf8");
+        console.log("hello!");
+        notesData = JSON.parse(notesData);
+
+    } catch (err) {
+        console.log("\n error (in app.get.catch):");
+        console.log(err);
+    }
+    res.json(notesData);
+});
+
