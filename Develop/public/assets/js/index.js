18c97b60c0ec72b1510b91f75602dc849a4c164d
dcashdallas <dcashdallas81@yahoo.com>
2020-06-15 19:09:43 -0500

    updating data for index, render the list of note titles

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -125,19 +125,7 @@ var renderNoteList = function (notes) {
   $noteList.append(noteListItems);
 };
 
-// Gets notes from the db and renders them to the sidebar
-var getAndRenderNotes = function () {
-  return getNotes().then(function (data) {
-    renderNoteList(data);
-  });
-};
 
-$saveNoteBtn.on("click", handleNoteSave);
-$noteList.on("click", ".list-group-item", handleNoteView);
-$newNoteBtn.on("click", handleNewNoteView);
-$noteList.on("click", ".delete-note", handleNoteDelete);
-$noteTitle.on("keyup", handleRenderSaveBtn);
-$noteText.on("keyup", handleRenderSaveBtn);
 
 // Gets and renders the initial list of notes
 getAndRenderNotes();
