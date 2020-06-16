ebe446621f060afb46412cc1fce3bd5fdf8e3bf1
dcashdallas <dcashdallas81@yahoo.com>
2020-06-16 05:50:07 -0500

    updating additional js details

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = 8080;
+const port = 2001;
 const mainDir = path.join(__dirname, "/Develop/public");
 
 app.use(express.static('Develop/public'));
