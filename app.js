28526f02b7b02903389320c49679a73daf04dbf0
dcashdallas <dcashdallas81@yahoo.com>
2020-06-16 22:14:46 -0500

    creating heroku app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 2001;
+const port = process.env.PORT || 2001;
 const mainDir = path.join(__dirname, "/Develop/public");
 
 app.use(express.static('Develop/public'));
